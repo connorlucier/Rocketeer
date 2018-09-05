c1a8d1fcb8fcdef5e14fc5984672be69d78035da
Connor Lucier <connor@connorlucier.com>
2018-09-05 13:00:05 -0700

    second level, created obstacles, improved controls of ship

--- Rocketeer/Assets/Scripts/Rocket.cs
+++ Rocketeer/Assets/Scripts/Rocket.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField]
+    float rcsThrust;
+
+    [SerializeField]
+    float mainThrust;
+
     Rigidbody rigidBody;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
-	}
+
+        if (rcsThrust <= 0)
+        {
+            rcsThrust = 150.0f;
+        }
+
+        if (mainThrust <= 0)
+        {
+            mainThrust = 250.0f;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,11 +34,19 @@ public class Rocket : MonoBehaviour {
         RotateShip();
 	}
 
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag != "Friendly")
+        {
+            print("Dead");
+        }
+    }
+
     private void ThrustShip()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * 20);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
         }
     }
 
@@ -32,12 +56,12 @@ public class Rocket : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward / (Time.deltaTime * 40));
+            transform.Rotate(Vector3.forward * rcsThrust * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back / (Time.deltaTime * 40));
+            transform.Rotate(Vector3.back * rcsThrust * Time.deltaTime);
         }
 
         rigidBody.freezeRotation = false;
